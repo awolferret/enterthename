33ec88513de8cfb7111b74e166e763cf930e0902
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-07 16:33:22 +0300

    2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Список доступных команд:");
+            Console.WriteLine("setLogin - установить логин");
+            Console.WriteLine("setPassword - установить пароль");
+            Console.WriteLine("changeBackgroundColor - изменить цвет заднего фона");
+            Console.WriteLine("changeFontColor - изменить цвет шрифта");
+            Console.WriteLine("info - показывает информацию");
+            Console.WriteLine("esc - закрыть программу");
+            string userInput;
+            string login = "No Login";
+            string password = "No password";
+            string changeBackgroundColor = "StandartBackgroundColor";
+            string changeFontColor = "StandartFontColor";
+
+            do
+            {
+                Console.WriteLine("Введите команду");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "setLogin":
+                        Console.WriteLine("Ведите логин");
+                        login = Console.ReadLine();
+                        break;
+                    case "setPassword":
+                        Console.WriteLine("Ведите пароль");
+                        password = Console.ReadLine();
+                        break;
+                    case "changeBackgroundColor":
+                        Console.WriteLine("Доступные цвета:Blue,Red,Cyan");
+                        changeBackgroundColor = Console.ReadLine();
+
+                        if (changeBackgroundColor == "Blue")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                        }
+                        else if (changeBackgroundColor == "Red")
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Cyan;
+                        }
+                        break;
+                    case "changeFontColor":
+                        Console.WriteLine("Доступные цвета: Yellow,Green,Magenta");
+                        changeFontColor = Console.ReadLine();
+
+                        if (changeFontColor == "Yellow")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else if (changeFontColor == "Green")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                        }
+                        break;
+                    case "info":
+                        Console.WriteLine(login);
+                        Console.WriteLine(password);
+                        Console.WriteLine(changeBackgroundColor);
+                        Console.WriteLine(changeFontColor);
+                        break;
+                    default:
+                        Console.WriteLine("не распознано");
+                        break;
+                }
+            }
+            while (userInput != "esc");
+            Console.WriteLine("Завершение работы");
+        }
+    }
+}
